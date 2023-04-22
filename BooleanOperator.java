cb75ef880a0b5616fbc7aada6bfc3b91ba19bb4b
masoud <masoudrng2@gmail.com>
2023-04-22 15:11:00 +0330

    BooleanOperator

---
+++ BooleanOperator.java
@@ -0,0 +1,15 @@
+public class BooleanOperator {
+    public static void main(String[] args) {
+        boolean X=true;
+        boolean Y=false;
+        boolean W=true;
+        System.out.print("AND of w & x :");
+        System.out.println(X&&W);
+        System.out.print("OR of X and Y :");
+        System.out.println(X||Y);
+
+
+
+    }
+    
+}
