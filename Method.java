4e21dc75ae490fef7a454e2f8441563679ba3264
masoud <masoudrng2@gmail.com>
2023-04-22 15:21:53 +0330

    Method

---
+++ Method.java
@@ -0,0 +1,19 @@
+public class Method {
+    public static void main(String[] args) {
+        smile();
+
+System.out.println(adder(122, 90));
+
+
+    }
+static void smile(){
+        System.out.println(":-)");
+    }
+static int adder(int a,int b){
+    int sum=a+b;
+
+
+        return sum;
+    }
+
+}
