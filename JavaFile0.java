5bf576da9f37b631cde48de4c200d272d566236a
masoud <masoudrng2@gmail.com>
2023-04-23 23:40:32 +0330

    output

--- JavaFile0.java
+++
@@ -1,6 +0,0 @@
-public class JavaFile0 {
-    public public static void JavaFile0(String[] args) {
-        int X=12;
-        
-    }
-}
--- test.java
+++ test.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+    System.out.print("inset your Age:");
+    int A=inp.nextInt();
+    System.out.print("your Age is: "+A+'!');
+
         
     }
 }
