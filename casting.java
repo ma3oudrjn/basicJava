3c34221e00d7f2324670a62ee8ae3c5bbbbb4571
masoud <masoudrng2@gmail.com>
2023-04-24 11:52:57 +0330

    Create casting.java

---
+++ casting.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class casting{
+public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    int A=scanner.nextInt();
+    long B=(long)A;
+    System.out.println("_________________");
+    System.out.print(B);
+    System.out.println("_________________");
+
+B
+
+}
+
+
+
+
+}
