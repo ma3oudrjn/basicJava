64ab4c114be7246ffd5a16f01c4dd558f6335f0b
masoud <masoudrng2@gmail.com>
2023-04-24 12:49:06 +0330

    Update casting.java

--- casting.java
+++ casting.java
@@ -5,12 +5,26 @@ public static void main(String[] args) {
     Scanner scanner=new Scanner(System.in);
     int A=scanner.nextInt();
     long B=(long)A;
+
     System.out.println("_________________");
     System.out.print(B);
     System.out.println("_________________");
+    System.out.println(CasttoLong(A));
 
-B
 
+}
+/**
+ * @param A
+ * @return 
+ */
+static long CasttoLong(int A){
+    long X=(long)A;
+
+
+
+
+
+    return X;
 }
 
 
