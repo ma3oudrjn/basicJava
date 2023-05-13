8d3e736f7a58e50abc1ef3c6c4fc7f143c9a4299
masoud <masoudrng2@gmail.com>
2023-05-13 11:00:31 +0330

    ok

--- Method0.java
+++ Method0.java
@@ -1,6 +1,13 @@
 public class Method0 {
     public static void main(String[] args) {
+        int q=12;System.out.print(reint(q));
+      
         
+    }
+    static int reint (int a){
+        
+
+return a+1;
     }
     
 }
